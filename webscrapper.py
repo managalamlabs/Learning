e5b14e7cf6b1b1e66c78b9b0d46d6ae2ee0fb661
managalam labs <managalam.labs@gmail.com>
2017-10-07 20:08:12 +0530

    added web scrapping basic functionality

---
+++ webscrapper.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[11]:
+
+import urllib
+import urllib2
+
+def get_page_spoof(s):
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+    req = urllib2.Request(s, headers=hdr)
+    expet = 0
+    try:
+        page = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print 'HTTPError = ' + str(e.code)
+        expet = 1
+    except urllib2.URLError, e:
+        print 'URLError = ' + str(e.reason)
+        expet = 1
+    except httplib.HTTPException, e:
+        print 'HTTPException'
+        expet = 1
+    except Exception:
+        # import traceback
+        print 'generic exception: '
+        expet = 1
+    if expet == 0:
+        content = page.read()
+        page.close()
+        return content
+    else:
+        return 'no content for link ' + s
+    
+#fil=open('C:\\Users\\Dell\\Desktop\\managalamproject\\input\\ectime.txt','w')
+#fil.write(get_page_spoof('http://economictimes.indiatimes.com/'))
+pagesp=get_page_spoof('http://economictimes.indiatimes.com/')
+if pagesp.find('Google'):
+    print pagesp[pagesp.index('Google'):pagesp.index('Google')+25]
+
+
+# In[ ]:
+
+
+
